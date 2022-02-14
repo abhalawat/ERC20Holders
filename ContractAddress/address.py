09c29869c7561533e68e37406d9917c641bcd0e6
abhalawat <abhalawat@deqode.com>
2022-02-14 17:41:18 +0530

    Removed Variables

--- ContractAddress/address.py
+++ ContractAddress/address.py
@@ -1,9 +1,7 @@
-import json
 from web3 import Web3
-from web3.middleware import geth_poa_middleware
 import pymongo
 import ray
-from ray.util import inspect_serializability
+#from ray.util import inspect_serializability
 
 
 
@@ -12,27 +10,25 @@ def mongo(contractAddress, block):
     # client = pymongo.MongoClient(connection_url)
     # Database = client.get_database('Holders')
     Block_Address = pymongo.MongoClient('mongodb+srv://abhalawat:1234@cluster0.hnxq5.mongodb.net/Holders?retryWrites=true&w=majority').get_database('Holders').Block_Address
-    queryObject = {
-    'contractAddress': contractAddress,
-    'Block': block
-    }
     if Block_Address.find_one({'contractAddress': contractAddress}) == None:
-        Block_Address.insert_one(queryObject)
+        Block_Address.insert_one({
+            'contractAddress': contractAddress,
+            'Block': block
+                })
 
 
 
 @ray.remote
 def contract(_block):
-    infura_url= "wss://eth-mainnet.alchemyapi.io/v2/NMMzTK9vae0CA0DrxtR_TiqCHkf3qkqD"
-    web3 = Web3(Web3.WebsocketProvider(infura_url))
-    web3.middleware_onion.inject(geth_poa_middleware, layer=0) 
+    #infura_url= "wss://eth-mainnet.alchemyapi.io/v2/NMMzTK9vae0CA0DrxtR_TiqCHkf3qkqD"
+    web3 = Web3(Web3.WebsocketProvider("wss://eth-mainnet.alchemyapi.io/v2/NMMzTK9vae0CA0DrxtR_TiqCHkf3qkqD"))
     for i in range(len(web3.eth.get_block(_block).transactions)):
-        tx_hash = web3.eth.get_block(_block).transactions[i].hex()
-        address = web3.eth.getTransactionReceipt(tx_hash).contractAddress
+        #tx_hash = web3.eth.get_block(_block).transactions[i].hex()
+        address = web3.eth.getTransactionReceipt(web3.eth.get_block(_block).transactions[i].hex()).contractAddress
         #print("address ",address," txHash ",tx_hash)
         if address != None:
             mongo(address, _block)
-            return address
+            
 
 if __name__=="__main__":
     #inspect_serializability(contract, name="contract")
@@ -43,12 +39,9 @@ if __name__=="__main__":
     '''.format(len(ray.nodes()), ray.cluster_resources()['CPU']))
     infura_url= "wss://eth-mainnet.alchemyapi.io/v2/NMMzTK9vae0CA0DrxtR_TiqCHkf3qkqD"
     web3 = Web3(Web3.WebsocketProvider(infura_url))
-    web3.middleware_onion.inject(geth_poa_middleware, layer=0)
     latestBlock = web3.eth.get_block('latest').number
     del web3
     del infura_url
-    # futures = [contract.remote(block) for block in reversed(range(latestBlock))]
-    # print(ray.get(futures))
     ray.get([contract.remote(block) for block in reversed(range(latestBlock))])
     
 
